7184c72f0bb24467919c49bff4a4de6f0ec3a026
gastons25 <gaston.sanchez@kueskipay.com>
2023-05-08 12:02:16 -0600

    initial

---
+++ kong-shared.go
@@ -0,0 +1,12 @@
+
+
+package kongshared
+
+var MSGMAP = map[string]string {
+	"MSG0100": "User does not exists",
+	"MSG0101": "User already exists",
+	"MSG0200": "Invalid account id",
+	"MSG0201": "Invalid account id",
+}
+
+
